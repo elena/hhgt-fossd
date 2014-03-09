19cb52254630e54815ac48dce471d28679b97f99
Elena Williams <github@elena.net.au>
2014-03-09 23:28:50 +0800

    Added slides urls.

--- presentation/urls.py
+++ presentation/urls.py
@@ -6,5 +6,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', include('slides.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
