daba3885c15216151bfc6b950a6d4b52daa95b3d
Elena Williams <github@elena.net.au>
2014-03-09 16:30:43 +0800

    Added separate local settings pattern.

---
+++ presentation/settings/local.py
@@ -0,0 +1,23 @@
+import os
+from presentation.settings.base import *
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '!0gxy*sve7li-@z^5hca0a_e8)x6c-je4k#+i27po+^pur28k3'
+
+STATIC_ROOT = os.path.join(os.getcwd(), "static")
+
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(__file__), "../../shower"),
+    os.path.join(os.path.dirname(__file__), "../../static"),
+)
