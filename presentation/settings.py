c1d305af3c38cd109e3a9cb1c790fe147a66278e
Elena Williams <github@elena.net.au>
2014-03-09 17:02:25 +0800

    Fixed some relative paths for changed pattern and making collected static as static/public.

--- presentation/settings.py
+++ presentation/settings.py
@@ -12,19 +12,20 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))+'/..'
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '!0gxy*sve7li-@z^5hca0a_e8)x6c-je4k#+i27po+^pur28k3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-TEMPLATE_DIRS = [
-    os.path.join(BASE_DIR, 'templates'),
-]
-
 ALLOWED_HOSTS = []
+
 
 # Application definition
 
@@ -52,6 +53,17 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'presentation.urls'
 
 WSGI_APPLICATION = 'presentation.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
--- presentation/settings/local.py
+++ presentation/settings/local.py
@@ -15,9 +15,9 @@ DATABASES = {
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '!0gxy*sve7li-@z^5hca0a_e8)x6c-je4k#+i27po+^pur28k3'
 
-STATIC_ROOT = os.path.join(os.getcwd(), "static")
+STATIC_ROOT = os.path.join(os.getcwd(), "static/public")
 
 STATICFILES_DIRS = (
-    os.path.join(os.path.dirname(__file__), "../../shower"),
-    os.path.join(os.path.dirname(__file__), "../../static"),
+    os.path.join(os.path.dirname(__file__), "../../../shower"),
+    os.path.join(os.path.dirname(__file__), "../../../static"),
 )
