8593a76dcb1c6d7ce42bd93b26ff6ec22865fe50
Elena Williams <github@elena.net.au>
2014-03-09 16:53:03 +0800

    Rearranged slide list display to best view this stage of process.

--- slides/admin.py
+++ slides/admin.py
@@ -1,31 +1,54 @@
+from django import forms
 from django.contrib import admin
 from .models import Talk, Section, Slide
+
 
 admin.site.register(Talk)
 admin.site.register(Section)
 
-class SlideAdmin(admin.ModelAdmin):
 
-    extra = 1
+class SlideAdminForm(forms.ModelForm):
+
+    class Meta:
+        model = Slide
+
+    def __init__(self):
+        widgets = {
+           'content' : forms.Textarea(attrs={'class':'cke_editable cke_editable_inline cke_contents_ltr'}),
+        }
+
+
+class SlideAdmin(admin.ModelAdmin):
+    form = SlideAdminForm
+    save_on_top = True
     list_display = [
+        'content',
+        'header',
+        'slide_id',
         'talk',
         'section',
         'style',
         'colour_scheme',
-        'version',
-        'slide_id',
-        'header',
-        'content',
+        'versionA',
+        'versionB',
+        'versionC',
         'order',
         'is_enabled',
     ]
     list_editable = [
+        'content',
         'section',
         'style',
         'colour_scheme',
-        'version',
+        'versionA',
+        'versionB',
+        'versionC',
         'slide_id',
         'header',
+        'is_enabled',
+    ]
+    list_display_links = [
+        'talk',
     ]
 
 
