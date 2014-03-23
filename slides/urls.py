73f991169ac51cd49a8eac75e7f0bbf10ee87e22
Elena Williams <github@elena.net.au>
2014-03-23 13:12:43 +0800

    Added section slides view.

--- slides/urls.py
+++ slides/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import patterns, url
 from slides import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.ListView.as_view(), name='index')
+    url(r'^section/(?P<pk>\d+)/$', views.SectionDetailView.as_view(),
+        name='section_detail_view'),
+
+    url(r'^$', views.ListView.as_view(), name='index'),
 )
--- slides/views.py
+++ slides/views.py
@@ -1,8 +1,21 @@
 from django.views import generic
-from .models import Slide
+from .models import Slide, Section
 
 class ListView(generic.ListView):
 
     model = Slide
     context_object_name = 'slides'
     template_name = 'base.html'
+
+
+class SectionDetailView(generic.DetailView):
+
+    model = Section
+    template_name = 'base.html'
+    slug_field = 'pk'
+    slug_url_kwarg = 'pk'
+
+    def get_context_data(self, **kwargs):
+        context = super(SectionDetailView, self).get_context_data(**kwargs)
+        context['slides'] = Slide.objects.filter(section=self.get_object())
+        return context
